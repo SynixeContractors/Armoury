914f270e2582e345c5f4e1b9b95e74e9980c4447
Patryk Kaniewski <p7tryk@gmail.com>
2025-08-25 13:07:04 +0200

    scopes: add DMS (burris XTR) w/o adjustments

---
+++ addons/scopes/CfgWeapons.hpp
@@ -0,0 +1,23 @@
+class CfgWeapons {
+    class optic_DMS;
+    class optic_DMS_ghex_F;
+    class optic_DMS_weathered_F;
+    class optic_DMS_snake_lxWS;
+
+    class CLASS(optic_DMS): optic_DMS {
+        displayName = "Burris XTR III";
+        MACRO_NULL_SCOPE;
+    };
+    class CLASS(optic_DMS_ghex): optic_DMS_ghex_F {
+        displayName = "Burris XTR III (Green Hex)";
+        MACRO_NULL_SCOPE;
+    };
+    class CLASS(optic_DMS_weathered): optic_DMS_weathered_F {
+        displayName = "Burris XTR III (Old)";
+        MACRO_NULL_SCOPE;
+    };
+    class CLASS(optic_DMS_snake): optic_DMS_snake_lxWS {
+        displayName = "Burris XTR III (Snake)";
+        MACRO_NULL_SCOPE;
+    };
+};
