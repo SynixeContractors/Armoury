aec44116aea2138f34061ebdeab2818ac723ce61
Patryk Kaniewski <p7tryk@gmail.com>
2025-08-31 11:39:59 +0200

    ammunition: add 9.3mm belts

---
+++ addons/ammunition/bi/93x64.hpp
@@ -0,0 +1,30 @@
+// 9.3 brenneke
+
+// HK121 belts
+// base game mag is 120 rounds even tho it says 150
+class CLASS(120Rnd_93x64_Ball): 150Rnd_93x64_Mag {
+    ammo = QCLASS(93x64_Ball);
+    displayName = "9.3x64 120Rnd Belt (TE4) (Ball)";
+    displayNameShort = "Ball TE4";
+    descriptionShort = "9.3x64 Ball Tracer Every 4";
+    tracersEvery = 4;
+};
+class CLASS(120Rnd_93x64_Ball_Tracer): CLASS(120Rnd_93x64_Ball) {
+    displayName = "9.3x64 120Rnd Belt (T) (Ball)";
+    displayNameShort = "Ball Tracer";
+    descriptionShort = "9.3x64 Ball Tracer";
+    tracersEvery = 1;
+};
+class CLASS(120Rnd_93x64_EPR): CLASS(120Rnd_93x64_Ball) {
+    ammo = QCLASS(93x64_EPR);
+    displayName = "9.3x64 120Rnd Belt (TE4) (EPR)";
+    displayNameShort = "EPR TE4";
+    descriptionShort = "9.3x64 EPR Tracer Every 4";
+};
+class CLASS(120Rnd_93x64_AP): CLASS(120Rnd_93x64_EPR) {
+    ammo = QCLASS(93x64_AP);
+    displayName = "9.3x64 120Rnd Belt (TE4) (AP)";
+    displayNameShort = "AP TE4";
+    descriptionShort = "9.3x64 AP Tracer Every 4";
+};
+
