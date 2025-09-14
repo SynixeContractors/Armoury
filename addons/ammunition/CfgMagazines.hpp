3b2b625dcfc4c8ced890e6635d31c7ce4576be59
Patryk Kaniewski <p7tryk@gmail.com>
2025-09-14 13:51:16 +0200

    ammunition: add .50cal

--- addons/ammunition/CfgMagazines.hpp
+++ addons/ammunition/CfgMagazines.hpp
@@ -28,6 +28,7 @@ class CfgMagazines {
     class 10Rnd_338_Mag;
     class 7Rnd_408_Mag;
     class 150Rnd_93x64_Mag;
+    class 5Rnd_127x108_Mag;
 
     // Handgun baseclasses
     class 16Rnd_9x21_Mag;
@@ -49,5 +50,6 @@ class CfgMagazines {
     #include "bi\338LM.hpp"
     #include "bi\408CT.hpp"
     #include "bi\93x64.hpp"
+    #include "bi\127x108.hpp"
     #include "bi\vehicle.hpp"
 };
