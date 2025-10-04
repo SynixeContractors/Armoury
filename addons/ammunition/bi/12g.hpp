ca81b0d7d9173c7b66ca4f0e9f4cff336adcb658
Brett Mayson <brett@mayson.io>
2025-10-04 12:17:42 -0600

    single zip ci, fix magnum

--- addons/ammunition/bi/12g.hpp
+++ addons/ammunition/bi/12g.hpp
@@ -7,6 +7,21 @@ class CLASS(2Rnd_P_000): 2Rnd_12Gauge_Pellets {
     mass = 0.6;
 };
 
+// 8Rnd M1014
+class CLASS(8Rnd_P_000): CA_Magazine {
+    author = ECSTRING(main,Author);
+    scope = 2;
+    displayName = "8Rnd M1014 (Magnum)";
+    displayNameShort = "#00 Magnum";
+    descriptionShort = "#00 Magnum";
+    ammo = QCLASS(P_12G_000);
+    count = 8;
+    initSpeed = 385;
+    picture = "\CUP\Weapons\CUP_Weapons_Ammunition\data\ui\m_12gauge_ca.paa";
+    mass = 8;
+    reloadaction = "CUP_GestureReloadM1014_8Rnd";
+};
+
 class CLASS(8Rnd_S_AP20): CA_Magazine {
     MACRO_SCOPE;
     ammo = QCLASS(S_12G_AP20);
@@ -16,6 +31,7 @@ class CLASS(8Rnd_S_AP20): CA_Magazine {
     count = 8;
     picture = "\a3\Weapons_F_Enoch\MagazineProxies\data\UI\icon_2rnd_12gauge_slugs_CA.paa";
     mass = 8;
+    reloadaction = "CUP_GestureReloadM1014_8Rnd";
 };
 
 class CLASS(8Rnd_Smoke_M1014): CLASS(8Rnd_S_AP20) {
@@ -26,7 +42,7 @@ class CLASS(8Rnd_Smoke_M1014): CLASS(8Rnd_S_AP20) {
 };
 
 // 6Rnd M1014
-class CLASS(6Rnd_P_000) {
+class CLASS(6Rnd_P_000): CLASS(8Rnd_P_000) {
     displayName = "6Rnd M1014 (Magnum)";
     displayNameShort = "#00 Magnum";
     descriptionShort = "#00 Magnum Shells";
