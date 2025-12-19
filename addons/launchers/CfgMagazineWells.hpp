809d5262445f6489fa4cd5f6618680fdde57124f
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-19 16:33:10 +0100

    launchers: NSAM stub

--- addons/launchers/CfgMagazineWells.hpp
+++ addons/launchers/CfgMagazineWells.hpp
@@ -1,19 +1,16 @@
 class CfgMagazineWells {
     class Verona_Long {
         ADDON[] += {
-            QCLASS(Verona_AA),
             QCLASS(Stinger_LOAL),
         };
     };
     class CBA_Carl_Gustaf {
         ADDON[] += {
-            QCLASS(Verona_AA),
             QCLASS(Stinger_LOAL),
         };
     };
     class RPG32 {
         ADDON[] += {
-            QCLASS(Verona_AA),
             QCLASS(Stinger_LOAL),
         };
     };
--- addons/launchers/CfgMagazines.hpp
+++ addons/launchers/CfgMagazines.hpp
@@ -1,16 +1,5 @@
 class CfgMagazines {
-    class Titan_AA;
-    class CLASS(air_to_air_magazine): Titan_AA {
-        ammo = QCLASS(air_to_air_missile);
-        displayName = "Super AA missile";
-        displayNameShort = "SAM";
-    };
     class Vorona_HE;
-    class CLASS(Verona_AA): Vorona_HE {
-        ammo = QCLASS(M_verona_airburst);
-        displayName = "Super AA missile";
-        displayNameShort = "SAM";
-    };
     class CLASS(Stinger_LOAL): Vorona_HE {
         ammo = QCLASS(M_Stinger_LOAL);
         displayName = "Stinger LOAL";
