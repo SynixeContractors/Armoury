4349983a2fa67c7a241c96ecc3fc612303529c03
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-30 18:43:50 +0100

    compatace: add aegis sources

--- addons/compat_ace/aegis/CfgWeapons.hpp
+++ addons/compat_ace/aegis/CfgWeapons.hpp
@@ -1,53 +1,51 @@
 class CfgWeapons {
-  class Rifle_Base_F;
-  class Rifle_Long_Base_F;
-  class Rifle_Short_Base_F;
-  class Pistol_Base_F;
-  class sgun_Mp153_black_F: Rifle_Base_F {
-    ACE_barrelTwist = SHOTGUN_BARREL_TWIST;
-    ACE_barrelLength = 610.0;
-  };
-  class sgun_Mp153_classic_F: Rifle_Base_F {
-    ACE_barrelTwist = SHOTGUN_BARREL_TWIST;
-    ACE_barrelLength = 750.0;
-  };
-  class Aegis_arifle_M16A4_base_F: Rifle_Base_F {
-    ACE_barrelTwist = 177.8; //1 in 7in
-    ACE_barrelLength = 508.0;
-  };
-  class Aegis_MMG_FNMAG_Base: Rifle_Long_Base_F {
-    ACE_barrelTwist = 305; // like m14?
-    ACE_barrelLength = 630.0;
-  };
-  class Aegis_MMG_FNMAG_240_F: Aegis_MMG_FNMAG_Base {
-    ACE_barrelLength = 528.0;
-  };
-  class sgun_M4_F: Rifle_Short_Base_F {
-    ACE_barrelTwist = SHOTGUN_BARREL_TWIST;
-    ACE_barrelLength = 470.0;
-  };
-  class GL_M32_F: Rifle_Base_F {
-    ACE_barrelLength = 300.0;
-  };
-  class hgun_G17_F: Pistol_Base_F {
-    ACE_barrelTwist = 254; // like 9mm pistols
-    ACE_barrelLength = 114.0;
-  };  
-  class hgun_Mk26_F: Pistol_Base_F {
-    ACE_barrelTwist = 482; // copied from .50 DE
-    ACE_barrelLength = 152.0;
-  };
-  class Aegis_hgun_Pistol_R57_F: Pistol_Base_F {
-    ACE_barrelTwist = 152; //like MP7
-    ACE_barrelLength = 125.0;
-  };
-  class sgun_KSG_F: Rifle_Short_Base_F {
-    ACE_barrelTwist = SHOTGUN_BARREL_TWIST;
-    ACE_barrelLength = 470.0;
-  };
-  class GM6_base_F: Rifle_Long_Base_F {
-    //copied from ace which applies it on weapon not base
-    ACE_barrelLength = 730;
-    ACE_barrelTwist = 381;
-  };
+    class Rifle_Base_F;
+    class Rifle_Long_Base_F;
+    class Rifle_Short_Base_F;
+    class Pistol_Base_F;
+    class sgun_Mp153_black_F: Rifle_Base_F {
+        // https://web.archive.org/web/20140228123347/http://imzcorp.com/en/company/95.html
+        // in lengths: 610, 660, 710, 750
+        ACE_barrelLength = 610.0;
+    };
+    class sgun_Mp153_classic_F: Rifle_Base_F {
+        ACE_barrelLength = 750.0;
+    };
+    class Aegis_arifle_M16A4_base_F: Rifle_Base_F {
+        ACE_barrelTwist = 177.8; // 1 in 7in like modern AR15
+        ACE_barrelLength = 508.0; // 20 in
+        ace_twistDirection = 1;
+    };
+    class Aegis_MMG_FNMAG_Base: Rifle_Long_Base_F {
+        // https://fnherstal.com/app/uploads/technical-data-fn-mag-1.pdf
+        ACE_barrelTwist = 305.0; // 1 in 12in
+        ACE_barrelLength = 630.0;
+        ace_twistDirection = 1;
+    };
+    class Aegis_MMG_FNMAG_240_F: Aegis_MMG_FNMAG_Base {
+        // looks like 240L which is 4 in shorter barrel
+        ACE_barrelLength = 528.4;
+    };
+    class sgun_M4_F: Rifle_Short_Base_F {
+        //https://www.benellile.com/shotguns/m4-tactical-shotguns
+        ACE_barrelLength = 469.9; // 18.5 in
+    };
+    class hgun_G17_F: Pistol_Base_F {
+        ACE_barrelTwist = 250; // 1 in 9.84 in
+        ACE_barrelLength = 114.0; //4.49 in
+    };
+    class hgun_Mk26_F: Pistol_Base_F {
+        // https://shopkahrfirearmsgroup.com/mri-technical-data/
+        ACE_barrelTwist = 457.0; // 1 in 18 in
+        ACE_barrelLength = 152.0; // 6 in
+    };
+    class Aegis_hgun_Pistol_R57_F: Pistol_Base_F {
+        // https://ruger.com/products/ruger57/specSheets/16401.html
+        ACE_barrelTwist = 228.6; // 1 in 9in
+        ACE_barrelLength = 125.476; // 4.94 in
+    };
+    class sgun_KSG_F: Rifle_Short_Base_F {
+        // https://www.keltecweapons.com/wp-content/uploads/2025/09/KEL_Shotgun_Manual_UPDATE_v1_FINAL_COMBINED.pdf
+        ACE_barrelLength = 470.0; // 18.5 in
+    };
 };
