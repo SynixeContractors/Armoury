0627b057c8ed92aeb06330b3baea4b6bcde2a37f
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-31 14:57:40 +0100

    compatace: move western sahara

--- addons/compat_ace/CfgWeapons.hpp
+++ addons/compat_ace/CfgWeapons.hpp
@@ -1,14 +1,13 @@
 class CfgWeapons {
-  class Rifle_Base_F;
-  class Rifle_Long_Base_F;
-  class arifle_XMS_Base_lxWS;
-  class arifle_XMS_M_lxWS: arifle_XMS_Base_lxWS {
-    ACE_barrelLength = 450.0;
-  };
-  class sgun_HunterShotgun_01_base_F: Rifle_Long_Base_F {
-    ACE_barrelLength = 700.0;
-  };
-  class sgun_HunterShotgun_01_sawedoff_base_F: sgun_HunterShotgun_01_base_F {
-    ACE_barrelLength = 300.0; // guess
-  };
+    class Rifle_Long_Base_F;
+    class arifle_XMS_Base_lxWS;
+    class arifle_XMS_M_lxWS: arifle_XMS_Base_lxWS {
+        ACE_barrelLength = 450.0;
+    };
+    class sgun_HunterShotgun_01_base_F: Rifle_Long_Base_F {
+        ACE_barrelLength = 700.0;
+    };
+    class sgun_HunterShotgun_01_sawedoff_base_F: sgun_HunterShotgun_01_base_F {
+        ACE_barrelLength = 300.0; // guess
+    };
 };
