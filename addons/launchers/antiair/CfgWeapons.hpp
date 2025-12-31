d1d13318361bfd09c6fb3eb1d36e30d0fffeacd9
Patryk Kaniewski <p7tryk@gmail.com>
2025-12-31 22:34:44 +0100

    launchers: change NSAM standalone to LOBL

--- addons/launchers/antiair/CfgWeapons.hpp
+++ addons/launchers/antiair/CfgWeapons.hpp
@@ -4,20 +4,19 @@ class CfgWeapons {
     class launch_Vorona_base_F: Launcher_Base_F {
       magazineWell[] += {"Verona_Long"};
     };
-    class launch_NLAW_F;
-    class CLASS(launch_NSAM_F): launch_NLAW_F {
+    class launch_B_Titan_F;
+    class CLASS(launch_NSAM_F): launch_B_Titan_F {
       scope = 2;
       scopeArsenal = 2;
       baseWeapon = QCLASS(launch_NSAM_F);
-      canLock = 0;
-      displayName = "NSAM";
+      displayname = "NSAM";
       descriptionShort = "Next Generation Surface to Air Missile";
       modes[] = {"Single"};
-      model = "\z\ace\addons\dragon\models\ace_m47_magazine.p3d";
-      modelOptics = "";
-      // Potentially add different texture
-      picture = "\z\ace\addons\dragon\data\m47_dragon_item_ca.paa";
-      UiPicture = "\A3\Weapons_F\Data\UI\icon_aa_CA.paa";
+      //find a different model and/or retexture
+      modelOptics = "\z\ace\addons\javelin\data\reticle_titan.p3d";
+      model = "\A3\weapons_F\launchers\nlaw\nlaw_F.p3d";
+      picture = "\A3\weapons_f\launchers\nlaw\data\UI\gear_nlaw_ca.paa";
+      UiPicture = "\A3\Weapons_F\Data\UI\icon_at_CA.paa";
       class WeaponSlotsInfo: WeaponSlotsInfo {
         mass = 50;
       };
@@ -26,7 +25,7 @@ class CfgWeapons {
       scope = 1;
       scopeArsenal = 1;
       baseWeapon = QCLASS(launch_NSAM_F);
-      magazines[] = {QCLASS(NSAM_LOAL)};
+      magazines[] = {QCLASS(NSAM_LOBL)};
       class EventHandlers {
         fired = "call CBA_fnc_firedDisposable";
       };
