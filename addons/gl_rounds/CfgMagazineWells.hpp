076d7d92592d0ddc1a52f73084ec4b101968a138
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-01 15:10:47 +0100

    gl_rounds: 6rnd stun and texture

--- addons/gl_rounds/CfgMagazineWells.hpp
+++ addons/gl_rounds/CfgMagazineWells.hpp
@@ -4,4 +4,9 @@ class CfgMagazineWells {
             QCLASS(1Rnd_Stun_Grenade),
         };
     };
+    class CBA_40mm_M203_6rnds {
+        ADDON[] = {
+            QCLASS(6Rnd_Stun_Grenade),
+        };
+    };
 };
--- addons/gl_rounds/CfgMagazines.hpp
+++ addons/gl_rounds/CfgMagazines.hpp
@@ -1,8 +1,14 @@
 class CfgMagazines {
     class 1Rnd_SmokeYellow_Grenade_shell;
     class CLASS(1Rnd_Stun_Grenade): 1Rnd_SmokeYellow_Grenade_shell {
-        displayName = "XM748 Stun Grenade";
+        displayName = "XM748 Stun Grenade (Aerial Burst)";
         displayNameShort = "Stun (0.8s Fuze)";
         ammo = QCLASS(G_40mm_Stun);
+        picture = QPATHTOF(data\gl_stun.paa);
+    };
+    class CLASS(6Rnd_Stun_Grenade): CLASS(1Rnd_Stun_Grenade) {
+        displayName = "6Rnd XM748 Stun Grenade (Aerial Burst)";
+        count = 6;
+        mass = 24;
     };
 };
