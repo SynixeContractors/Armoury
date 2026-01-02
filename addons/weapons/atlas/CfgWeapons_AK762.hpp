c7d137d080b30b02c43bcf61ffbf83f86fe1a218
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-02 18:50:00 +0100

    weapons: AK15 Sand

--- addons/weapons/atlas/CfgWeapons_AK762.hpp
+++ addons/weapons/atlas/CfgWeapons_AK762.hpp
@@ -1,3 +1,4 @@
+//Green
 class arifle_AK12_F;
 class CLASS(arifle_AK15_green): arifle_AK12_F {
     displayName = "AK-15 (Green)";
@@ -21,4 +22,25 @@ class CLASS(arifle_AK15_GL_green): arifle_AK12_GL_F {
     displayName = "AK-15 GL (Green)";
     baseWeapon = QCLASS(arifle_AK15_GL_green);
     AK_GL_GREEN;
+};
+//Sand
+class CLASS(arifle_AK15_sand): arifle_AK12_F {
+    displayName = "AK-15 (Sand)";
+    baseWeapon = QCLASS(arifle_AK15_sand);
+    AK_SAND;
+};
+class CLASS(arifle_AK15K_sand): arifle_AK12U_F {
+    displayName = "AK-15K (Sand)";
+    baseWeapon = QCLASS(arifle_AK15K_sand);
+    AKK_SAND;
+};
+class CLASS(arifle_RPK15_sand): arifle_RPK12_F {
+    displayName = "RPK-15 (Sand)";
+    baseWeapon = QCLASS(arifle_RPK15_sand);
+    RPK_SAND;
+};
+class CLASS(arifle_AK15_GL_sand): arifle_AK12_GL_F {
+    displayName = "AK-15 GL (Sand)";
+    baseWeapon = QCLASS(arifle_AK15_GL_sand);
+    AK_GL_SAND;
 };
--- addons/weapons/atlas/script_macros.hpp
+++ addons/weapons/atlas/script_macros.hpp
@@ -17,3 +17,23 @@ hiddenSelectionsTextures[] = {"\A3_Atlas\Weapons_F_Atlas\Rifles\NCAR15\Data\NCAR
 picture = "\A3_Atlas\Weapons_F_Atlas\Rifles\NCAR15\Data\UI\icon_arifle_NCAR15_GL_F_X_CA.paa"; \
 hiddenSelectionsMaterials[] = {"\A3_Atlas\Weapons_F_Atlas\Rifles\NCAR15\Data\NCAR_F_1.rvmat","\A3\weapons_f_exp\Rifles\AK12\Data\AK12_F_2.rvmat","\A3\weapons_f_exp\Rifles\AK12\Data\AK12_GL_F.rvmat"}; \
 hiddenSelectionsTextures[] = {"\A3_Atlas\Weapons_F_Atlas\Rifles\NCAR15\Data\NCAR_ak12_1_co.paa","\A3_Atlas\Weapons_F_Atlas\Rifles\NCAR15\Data\NCAR_ak12_2_co.paa","\A3_Atlas\Weapons_F_Atlas\Rifles\NCAR15\Data\NCAR15_ncar15_gl_co.paa"}
+
+#define AK_SAND \
+picture = "\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\UI\icon_arifle_AK12_tan_F_X_CA.paa"; \
+hiddenSelectionsMaterials[] = {"\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_1.rvmat","\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_2.rvmat"}; \
+hiddenSelectionsTextures[] = {"\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_1_tan_co.paa","\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_2_tan_co.paa"}
+
+#define RPK_SAND \
+/*missing image*/ \
+hiddenSelectionsMaterials[] = {"\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_1.rvmat","\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_2.rvmat","\A3\Weapons_F_Enoch\Rifles\AK12\Data\AK12_RPK12.rvmat"}; \
+hiddenSelectionsTextures[] = {"\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_1_tan_co.paa","\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_2_tan_co.paa","\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_4_tan_co.paa"}
+
+#define AKK_SAND \
+picture = "\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\UI\icon_arifle_AKU12_tan_F_X_CA.paa"; \
+hiddenSelectionsMaterials[] = {"\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_1.rvmat","\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_2.rvmat","\A3\Weapons_F_Enoch\Rifles\AK12\Data\AK12_RPK12.rvmat"}; \
+hiddenSelectionsTextures[] = {"\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_1_tan_co.paa","\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_2_tan_co.paa","\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_4_tan_co.paa"}
+
+#define AK_GL_SAND \
+picture = "\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\UI\icon_arifle_AK12_GL_tan_F_X_CA.paa"; \
+hiddenSelectionsMaterials[] = {"\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_1.rvmat","\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_F_2.rvmat","\A3\Weapons_F_Exp\Rifles\AK12\Data\AK12_GL_F.rvmat"}; \
+hiddenSelectionsTextures[] = {"\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_1_tan_co.paa","\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_2_tan_co.paa","\A3_Aegis\Weapons_F_Aegis\Rifles\AK12\Data\arifle_ak12_3_tan_co.paa"}
