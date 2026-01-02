1aeb6c8d5e9b76c61c692a23933cd06da55ccf3c
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-02 22:40:12 +0100

    weapons: 5.8mm AK camos

--- addons/weapons/atlas/CfgWeapons.hpp
+++ addons/weapons/atlas/CfgWeapons.hpp
@@ -2,4 +2,5 @@ class CfgWeapons {
     //5.45
     #include "CfgWeapons_AK545.hpp"
     #include "CfgWeapons_AK762.hpp"
+    #include "CfgWeapons_AK58.hpp"
 };
---
+++ addons/weapons/atlas/CfgWeapons_AK58.hpp
@@ -0,0 +1,88 @@
+class arifle_NCAR15_F;
+class arifle_NCAR15B_F;
+class arifle_NCAR15_MG_F;
+class arifle_NCAR15_GL_F;
+// Sand
+class CLASS(arifle_AK03_sand): arifle_NCAR15_F {
+    displayName = "QBZ-03 (Sand)";
+    baseWeapon = QCLASS(arifle_AK03_sand);
+    AK_SAND;
+};
+class CLASS(arifle_AK03K_sand): arifle_NCAR15B_F {
+    displayName = "QBZ-03B (Sand)";
+    baseWeapon = QCLASS(arifle_AK03K_sand);
+    AKK_SAND;
+};
+class CLASS(arifle_RPK03_sand): arifle_NCAR15_MG_F {
+    displayName = "QBZ-03 LSW (Sand)";
+    baseWeapon = QCLASS(arifle_RPK03_sand);
+    RPK_SAND;
+};
+class CLASS(arifle_AK03_GL_sand): arifle_NCAR15_GL_F {
+    displayName = "QBZ-03 GL (Sand)";
+    baseWeapon = QCLASS(arifle_AK03_GL_sand);
+    AK_GL_SAND;
+};
+// Black
+class CLASS(arifle_AK03_black): arifle_NCAR15_F {
+    displayName = "QBZ-03 (Black)";
+    baseWeapon = QCLASS(arifle_AK03_black);
+    AK_BLACK;
+};
+class CLASS(arifle_AK03K_black): arifle_NCAR15B_F {
+    displayName = "QBZ-03B (Black)";
+    baseWeapon = QCLASS(arifle_AK03K_black);
+    AKK_BLACK;
+};
+class CLASS(arifle_RPK03_black): arifle_NCAR15_MG_F {
+    displayName = "QBZ-03 LSW (Black)";
+    baseWeapon = QCLASS(arifle_RPK03_black);
+    RPK_BLACK;
+};
+class CLASS(arifle_AK03_GL_black): arifle_NCAR15_GL_F {
+    displayName = "QBZ-03 GL (Black)";
+    baseWeapon = QCLASS(arifle_AK03_GL_black);
+    AK_GL_BLACK;
+};
+// Lush
+class CLASS(arifle_AK03_lush): arifle_NCAR15_F {
+    displayName = "QBZ-03 (Lush)";
+    baseWeapon = QCLASS(arifle_AK03_lush);
+    AK_LUSH;
+};
+class CLASS(arifle_AK03K_lush): arifle_NCAR15B_F {
+    displayName = "QBZ-03B (Lush)";
+    baseWeapon = QCLASS(arifle_AK03K_lush);
+    AKK_LUSH;
+};
+class CLASS(arifle_RPK03_lush): arifle_NCAR15_MG_F {
+    displayName = "QBZ-03 LSW (Lush)";
+    baseWeapon = QCLASS(arifle_RPK03_lush);
+    RPK_LUSH;
+};
+class CLASS(arifle_AK03_GL_lush): arifle_NCAR15_GL_F {
+    displayName = "QBZ-03 GL (Lush)";
+    baseWeapon = QCLASS(arifle_AK03_GL_lush);
+    AK_GL_LUSH;
+};
+// Arid
+class CLASS(arifle_AK03_arid): arifle_NCAR15_F {
+    displayName = "QBZ-03 (Arid)";
+    baseWeapon = QCLASS(arifle_AK03_arid);
+    AK_ARID;
+};
+class CLASS(arifle_AK03K_arid): arifle_NCAR15B_F {
+    displayName = "QBZ-03B (Arid)";
+    baseWeapon = QCLASS(arifle_AK03K_arid);
+    AKK_ARID;
+};
+class CLASS(arifle_RPK03_arid): arifle_NCAR15_MG_F {
+    displayName = "QBZ-03 LSW (Arid)";
+    baseWeapon = QCLASS(arifle_RPK03_arid);
+    RPK_ARID;
+};
+class CLASS(arifle_AK03_GL_arid): arifle_NCAR15_GL_F {
+    displayName = "QBZ-03 GL (Arid)";
+    baseWeapon = QCLASS(arifle_AK03_GL_arid);
+    AK_GL_ARID;
+};
