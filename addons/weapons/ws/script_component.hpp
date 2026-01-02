4acb272405cfc714161a5265d5a21cfda41eec02
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-02 22:41:58 +0100

    weapons: vektor 762

---
+++ addons/weapons/ws/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT weapons_ws
+#define COMPONENT_BEAUTIFIED Weapons Western Sahara
+#include "\s\synixe_armoury\addons\main\script_mod.hpp"
+#include "\s\synixe_armoury\addons\main\script_macros.hpp"
