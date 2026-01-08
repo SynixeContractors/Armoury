3ede84d0cd21fed27ff4b30aa49c2d1c1814dd06
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-08 10:54:24 +0100

    compat_ace: remove upstreamed aegis

--- addons/compat_ace/aegis/script_component.hpp
+++
@@ -1,4 +0,0 @@
-#define COMPONENT compat_ace_aegis
-#define COMPONENT_BEAUTIFIED Compat ACE Aegis
-#include "\s\synixe_armoury\addons\main\script_mod.hpp"
-#include "\s\synixe_armoury\addons\main\script_macros.hpp"
