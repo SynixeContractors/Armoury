ed3e93da383d8c7b5ca576a007a01ed71211b1f2
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-10 09:22:35 +0100

    weapons: 556 G36

--- addons/weapons/aegis/CfgWeapons.hpp
+++ addons/weapons/aegis/CfgWeapons.hpp
@@ -3,7 +3,15 @@ class CfgWeapons {
     class Aegis_arifle_Velko_sand;
     class Aegis_arifle_VelkoR5_oak;
     class Aegis_arifle_VelkoR5_sand;
+    class arifle_G36C_F;
+    class arifle_G36C_Sand_F;
+    class arifle_G36_F;
+    class arifle_G36_Sand_F;
+    class arifle_G36_GL_F;
+    class arifle_G36_GL_Sand_F;
     class arifle_VelkoR5_GL_lxWS;
+
     #include "CfgWeapons_Vektor762.hpp"
     #include "CfgWeapons_Vektor556.hpp"
+    #include "CfgWeapons_G36.hpp"
 };
---
+++ addons/weapons/aegis/CfgWeapons_G36.hpp
@@ -0,0 +1,48 @@
+class CLASS(arifle_G36C): arifle_G36C_F {
+    baseWeapon = QCLASS(arifle_G36C);
+    displayName = "G36C (Black)";
+    magazines[] = {};
+    magazineWell[] = {
+        "CBA_556x45_G36",
+    };
+};
+class CLASS(arifle_G36C_sand): arifle_G36C_Sand_F {
+    baseWeapon = QCLASS(arifle_G36C_sand);
+    displayName = "G36C (Sand)";
+    magazines[] = {};
+    magazineWell[] = {
+        "CBA_556x45_G36",
+    };
+};
+class CLASS(arifle_G36): arifle_G36_F {
+    baseWeapon = QCLASS(arifle_G36);
+    displayName = "G36K (Black)";
+    magazines[] = {};
+    magazineWell[] = {
+        "CBA_556x45_G36",
+    };
+};
+class CLASS(arifle_G36_sand): arifle_G36_Sand_F {
+    baseWeapon = QCLASS(arifle_G36_sand);
+    displayName = "G36K (Sand)";
+    magazines[] = {};
+    magazineWell[] = {
+        "CBA_556x45_G36",
+    };
+};
+class CLASS(arifle_G36_GL): arifle_G36_GL_F {
+    baseWeapon = QCLASS(arifle_G36_GL);
+    displayName = "G36K GL (Black)";
+    magazines[] = {};
+    magazineWell[] = {
+        "CBA_556x45_G36",
+    };
+};
+class CLASS(arifle_G36_GL_sand): arifle_G36_GL_Sand_F {
+    baseWeapon = QCLASS(arifle_G36_GL_sand);
+    displayName = "G36K GL (Sand)";
+    magazines[] = {};
+    magazineWell[] = {
+        "CBA_556x45_G36",
+    };
+};
