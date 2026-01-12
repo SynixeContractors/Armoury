3e46d0e7c932d0c72aaf9e6fd3bfbb32f839551e
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-12 08:50:35 +0100

    weapons: move to weapons macro

--- addons/weapons/aegis/CfgWeapons_Vektor762.hpp
+++ addons/weapons/aegis/CfgWeapons_Vektor762.hpp
@@ -1,52 +1,27 @@
 class CLASS(arifle_Vektor762_long_oak): Aegis_arifle_Velko_oak {
     baseWeapon = QCLASS(arifle_Vektor762_long_oak);
     displayName = "STV-380 (Classic)";
-    DESCRIPTION_AR_762x39;
-    magazines[] = {};
-    magazineWell[] = {
-        "CBA_762x39_AK",
-        "CBA_762x39_RPK",
-    };
+    MAGAZINE_AKM;
 };
 class CLASS(arifle_Vektor762_long_sand): Aegis_arifle_Velko_sand {
     baseWeapon = QCLASS(arifle_Vektor762_long_sand);
     displayName = "STV-380 (Sand)";
-    DESCRIPTION_AR_762x39;
-    magazines[] = {};
-    magazineWell[] = {
-        "CBA_762x39_AK",
-        "CBA_762x39_RPK",
-    };
+    MAGAZINE_AKM;
 };
 class CLASS(arifle_Vektor762_oak): Aegis_arifle_VelkoR5_oak {
     baseWeapon = QCLASS(arifle_Vektor762_oak);
     displayName = "STV-215 (Classic)";
-    DESCRIPTION_AR_762x39;
-    magazines[] = {};
-    magazineWell[] = {
-       "CBA_762x39_AK",
-       "CBA_762x39_RPK",
-    };
+    MAGAZINE_AKM;
 };
 class CLASS(arifle_Vektor762_sand): Aegis_arifle_VelkoR5_sand {
     baseWeapon = QCLASS(arifle_Vektor762_sand);
     displayName = "STV-215 (Sand)";
-    DESCRIPTION_AR_762x39;
-    magazines[] = {};
-    magazineWell[] = {
-        "CBA_762x39_AK",
-        "CBA_762x39_RPK",
-    };
+    MAGAZINE_AKM;
 };
 class CLASS(arifle_Vektor762_GL_sand): arifle_VelkoR5_GL_lxWS {
     baseWeapon = QCLASS(arifle_Vektor762_GL_sand);
     displayName = "STV-215 GL (Sand)";
-    DESCRIPTION_AR_762x39;
-    magazines[] = {};
-    magazineWell[] = {
-        "CBA_762x39_AK",
-        "CBA_762x39_RPK",
-    };
+    MAGAZINE_AKM;
     hiddenSelectionsTextures[] = {
         "A3_Aegis\weapons_f_aegis\Rifles\Velko\data\galatr4_sand_co.paa",
         "A3_Aegis\weapons_f_aegis\Rifles\Velko\data\handguard_sand_co.paa",
