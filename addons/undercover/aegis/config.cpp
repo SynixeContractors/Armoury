6cb8bbb9f8984e74ec88c8f9c0af28c9ed97d8bf
Patryk Kaniewski <p7tryk@gmail.com>
2026-01-18 17:18:29 +0100

    Fix undercover warnings and milgp migration (#15)
    * undercover: fix warnings about missing weapons aegis
    
    * undercover: migrate milgp to synixe_milgp
    
    * undercover: fix config weapons[]

--- addons/undercover/aegis/config.cpp
+++ addons/undercover/aegis/config.cpp
@@ -4,7 +4,38 @@ class CfgPatches {
     class ADDON {
         name = QUOTE(COMPONENT);
         units[] = {};
-        weapons[] = {};
+        weapons[] = {
+            QCLASS(aegis_v_carrierrigkbt_01_holster_black_f_heavy),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_black_f_medium),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_black_f_light),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_cbr_f_heavy),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_cbr_f_medium),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_cbr_f_light),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_khk_f_heavy),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_khk_f_medium),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_khk_f_light),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_olive_f_heavy),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_olive_f_medium),
+            QCLASS(aegis_v_carrierrigkbt_01_holster_olive_f_light),
+            QCLASS(v_rangemaster_belt_heavy),
+            QCLASS(v_rangemaster_belt_medium),
+            QCLASS(v_rangemaster_belt_light),
+            QCLASS(v_rangemaster_belt_blk_heavy),
+            QCLASS(v_rangemaster_belt_blk_medium),
+            QCLASS(v_rangemaster_belt_blk_light),
+            QCLASS(v_rangemaster_belt_cbr_heavy),
+            QCLASS(v_rangemaster_belt_cbr_medium),
+            QCLASS(v_rangemaster_belt_cbr_light),
+            QCLASS(v_rangemaster_belt_khk_heavy),
+            QCLASS(v_rangemaster_belt_khk_medium),
+            QCLASS(v_rangemaster_belt_khk_light),
+            QCLASS(v_rangemaster_belt_oli_heavy),
+            QCLASS(v_rangemaster_belt_oli_medium),
+            QCLASS(v_rangemaster_belt_oli_light),
+            QCLASS(v_rangemaster_belt_tna_f_heavy),
+            QCLASS(v_rangemaster_belt_tna_f_medium),
+            QCLASS(v_rangemaster_belt_tna_f_light),
+        };
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "synixe_main",
--- addons/undercover/milgp/config.cpp
+++ addons/undercover/milgp/config.cpp
@@ -4,11 +4,60 @@ class CfgPatches {
     class ADDON {
         name = QUOTE(COMPONENT);
         units[] = {};
-        weapons[] = {};
+        weapons[] = {
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_aor1_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_aor1_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_aor1_light),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_aor2_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_aor2_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_aor2_light),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_blk_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_blk_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_blk_light),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_cb_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_cb_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_cb_light),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_khk_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_khk_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_khk_light),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_mc_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_mc_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_mc_light),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_rgr_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_rgr_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_rgr_light),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_wht_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_wht_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_assaulter_wht_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_aor1_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_aor1_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_aor1_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_aor2_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_aor2_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_aor2_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_blk_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_blk_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_blk_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_cb_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_cb_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_cb_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_khk_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_khk_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_khk_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_mc_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_mc_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_mc_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_rgr_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_rgr_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_rgr_light),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_wht_heavy),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_wht_medium),
+            QCLASS(synixe_mgp_vest_battle_belt_hgunner_wht_light),
+        };
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "synixe_main",
-            "milgp_vests_cfg"
+            "synixe_milgp_vests",
         };
         skipWhenMissingDependencies = 1;
         VERSION_CONFIG;
