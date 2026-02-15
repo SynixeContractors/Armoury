ee66d3656cea1bf1628e9359153fd96c252dec02
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-15 18:32:31 +0100

    ammunition: 545 metal mag (#17)

--- addons/ammunition/aegis/CfgMagazines.hpp
+++ addons/ammunition/aegis/CfgMagazines.hpp
@@ -1,6 +1,7 @@
 class CfgMagazines {
     class Aegis_60Rnd_545x39_Mag_F;
     class Aegis_45Rnd_545x39_Mag_F;
+    class 30Rnd_545x39_Steel_Mag_Green_F;
     #include "magazines\127x33.hpp"
     #include "magazines\545x39.hpp"
     #include "magazines\46x30.hpp"
