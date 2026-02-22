149af63ac9bc78dbe138564ce8eb2361c672d215
Brett Mayson <brett@mayson.io>
2026-02-22 00:35:25 -0600

    fix pistols

--- addons/ammunition/jca_m9a1/CfgMagazineWells.hpp
+++ addons/ammunition/jca_m9a1/CfgMagazineWells.hpp
@@ -1,10 +1,10 @@
 class CfgMagazineWells {
-    class JCA_M9A1_9x21 {
+    class JCA_M9A1_9x19 {
         ADDON[] = {
-            QCLASS(15Rnd_9x21_Ball_M9A1_JCA),
-            QCLASS(15Rnd_9x21_Ball_Tracer_M9A1_JCA),
-            QCLASS(15Rnd_9x21_EPR_M9A1_JCA),
-            QCLASS(15Rnd_9x21_AP_M9A1_JCA)
+            QCLASS(15Rnd_9x19_Ball_M9A1_JCA),
+            QCLASS(15Rnd_9x19_Ball_Tracer_M9A1_JCA),
+            QCLASS(15Rnd_9x19_EPR_M9A1_JCA),
+            QCLASS(15Rnd_9x19_AP_M9A1_JCA)
         };
     };
 };
--- addons/ammunition/jca_m9a1/CfgMagazines.hpp
+++ addons/ammunition/jca_m9a1/CfgMagazines.hpp
@@ -1,29 +1,29 @@
 class CfgMagazines {
-    class JCA_15Rnd_9x21_M9A1_Mag;
-    class CLASS(15Rnd_9x21_Ball_M9A1_JCA): JCA_15Rnd_9x21_M9A1_Mag {
+    class JCA_15Rnd_9x19_M9A1_Yellow_Mag;
+    class CLASS(15Rnd_9x19_Ball_M9A1_JCA): JCA_15Rnd_9x19_M9A1_Yellow_Mag {
         MACRO_SCOPE;
-        ammo = QCLASS(9x21_Ball);
+        ammo = QCLASS(9x19_Ball);
         descriptionShort = "9mm Ball Reload Tracer";
         displayName = "9mm 15Rnd M9A1 (Ball)";
         lastRoundsTracer = 2;
     };
-    class CLASS(15Rnd_9x21_Ball_Tracer_M9A1_JCA): JCA_15Rnd_9x21_M9A1_Mag {
+    class CLASS(15Rnd_9x19_Ball_Tracer_M9A1_JCA): JCA_15Rnd_9x19_M9A1_Yellow_Mag {
         MACRO_SCOPE;
-        ammo = QCLASS(9x21_Ball);
+        ammo = QCLASS(9x19_Ball);
         descriptionShort = "9mm Ball Tracer";
         displayName = "9mm 15Rnd M9A1 [T] (Ball)";
         tracersEvery = 1;
     };
-    class CLASS(15Rnd_9x21_EPR_M9A1_JCA): JCA_15Rnd_9x21_M9A1_Mag {
+    class CLASS(15Rnd_9x19_EPR_M9A1_JCA): JCA_15Rnd_9x19_M9A1_Yellow_Mag {
         MACRO_SCOPE;
-        ammo = QCLASS(9x21_EPR);
+        ammo = QCLASS(9x19_EPR);
         descriptionShort = "9mm Ball EPR";
         displayName = "9mm 15Rnd M9A1 (EPR)";
         lastRoundsTracer = 2;
     };
-    class CLASS(15Rnd_9x21_AP_M9A1_JCA): JCA_15Rnd_9x21_M9A1_Mag {
+    class CLASS(15Rnd_9x19_AP_M9A1_JCA): JCA_15Rnd_9x19_M9A1_Yellow_Mag {
         MACRO_SCOPE;
-        ammo = QCLASS(9x21_AP);
+        ammo = QCLASS(9x19_AP);
         descriptionShort = "9mm AP";
         displayName = "9mm 15Rnd M9A1 (AP)";
         lastRoundsTracer = 2;
