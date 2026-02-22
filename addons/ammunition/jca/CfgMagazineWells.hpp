c6f99d111e53dc41f5e231107bcdb712cd6e5506
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-22 17:45:59 +0100

    jca: 762 sand mags and fix magwells

--- addons/ammunition/jca/CfgMagazineWells.hpp
+++ addons/ammunition/jca/CfgMagazineWells.hpp
@@ -19,42 +19,24 @@ class CfgMagazineWells {
             QCLASS(30Rnd_556x45_AP_JCA_EMAG_Sand),
         };
     };
-    class CBA_762x51_HK417 {
+    class JCA_SR_762x51 {
         ADDON[] = {
             QCLASS(20Rnd_762x51_Ball_JCA_PMAG),
             QCLASS(20Rnd_762x51_Ball_Tracer_JCA_PMAG),
             QCLASS(20Rnd_762x51_EPR_JCA_PMAG),
             QCLASS(20Rnd_762x51_AP_JCA_PMAG),
+            QCLASS(20Rnd_762x51_Ball_JCA_PMAG_Sand),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_PMAG_Sand),
+            QCLASS(20Rnd_762x51_EPR_JCA_PMAG_Sand),
+            QCLASS(20Rnd_762x51_AP_JCA_PMAG_Sand),
             QCLASS(20Rnd_762x51_Ball_JCA_SMAG),
             QCLASS(20Rnd_762x51_Ball_Tracer_JCA_SMAG),
             QCLASS(20Rnd_762x51_EPR_JCA_SMAG),
             QCLASS(20Rnd_762x51_AP_JCA_SMAG),
-        };
-    };
-
-    class CBA_762x51_M14 {
-        ADDON[] = {
-            QCLASS(20Rnd_762x51_Ball_JCA_PMAG),
-            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_PMAG),
-            QCLASS(20Rnd_762x51_EPR_JCA_PMAG),
-            QCLASS(20Rnd_762x51_AP_JCA_PMAG),
-            QCLASS(20Rnd_762x51_Ball_JCA_SMAG),
-            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_SMAG),
-            QCLASS(20Rnd_762x51_EPR_JCA_SMAG),
-            QCLASS(20Rnd_762x51_AP_JCA_SMAG),
-        };
-    };
-
-    class CBA_762x51_SR25 {
-        ADDON[] = {
-            QCLASS(20Rnd_762x51_Ball_JCA_PMAG),
-            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_PMAG),
-            QCLASS(20Rnd_762x51_EPR_JCA_PMAG),
-            QCLASS(20Rnd_762x51_AP_JCA_PMAG),
-            QCLASS(20Rnd_762x51_Ball_JCA_SMAG),
-            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_SMAG),
-            QCLASS(20Rnd_762x51_EPR_JCA_SMAG),
-            QCLASS(20Rnd_762x51_AP_JCA_SMAG),
+            QCLASS(20Rnd_762x51_Ball_JCA_SMAG_Sand),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_SMAG_Sand),
+            QCLASS(20Rnd_762x51_EPR_JCA_SMAG_Sand),
+            QCLASS(20Rnd_762x51_AP_JCA_SMAG_Sand),
         };
     };
 };
