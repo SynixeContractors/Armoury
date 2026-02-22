585db739388db981a87b5c9cbc4c81f9925405fc
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-22 17:28:10 +0100

    jca: 556 and 762 mags

---
+++ addons/ammunition/jca/CfgMagazineWells.hpp
@@ -0,0 +1,60 @@
+class CfgMagazineWells {
+    class CBA_556x45_STANAG {
+        ADDON[] = {
+            QCLASS(30Rnd_556x45_Ball_JCA_PMAG),
+            QCLASS(30Rnd_556x45_Ball_Tracer_JCA_PMAG),
+            QCLASS(30Rnd_556x45_EPR_JCA_PMAG),
+            QCLASS(30Rnd_556x45_AP_JCA_PMAG),
+            QCLASS(30Rnd_556x45_Ball_JCA_PMAG_Sand),
+            QCLASS(30Rnd_556x45_Ball_Tracer_JCA_PMAG_Sand),
+            QCLASS(30Rnd_556x45_EPR_JCA_PMAG_Sand),
+            QCLASS(30Rnd_556x45_AP_JCA_PMAG_Sand),
+            QCLASS(30Rnd_556x45_Ball_JCA_EMAG),
+            QCLASS(30Rnd_556x45_Ball_Tracer_JCA_EMAG),
+            QCLASS(30Rnd_556x45_EPR_JCA_EMAG),
+            QCLASS(30Rnd_556x45_AP_JCA_EMAG),
+            QCLASS(30Rnd_556x45_Ball_JCA_EMAG_Sand),
+            QCLASS(30Rnd_556x45_Ball_Tracer_JCA_EMAG_Sand),
+            QCLASS(30Rnd_556x45_EPR_JCA_EMAG_Sand),
+            QCLASS(30Rnd_556x45_AP_JCA_EMAG_Sand),
+        };
+    };
+    class CBA_762x51_HK417 {
+        ADDON[] = {
+            QCLASS(20Rnd_762x51_Ball_JCA_PMAG),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_PMAG),
+            QCLASS(20Rnd_762x51_EPR_JCA_PMAG),
+            QCLASS(20Rnd_762x51_AP_JCA_PMAG),
+            QCLASS(20Rnd_762x51_Ball_JCA_SMAG),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_SMAG),
+            QCLASS(20Rnd_762x51_EPR_JCA_SMAG),
+            QCLASS(20Rnd_762x51_AP_JCA_SMAG),
+        };
+    };
+
+    class CBA_762x51_M14 {
+        ADDON[] = {
+            QCLASS(20Rnd_762x51_Ball_JCA_PMAG),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_PMAG),
+            QCLASS(20Rnd_762x51_EPR_JCA_PMAG),
+            QCLASS(20Rnd_762x51_AP_JCA_PMAG),
+            QCLASS(20Rnd_762x51_Ball_JCA_SMAG),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_SMAG),
+            QCLASS(20Rnd_762x51_EPR_JCA_SMAG),
+            QCLASS(20Rnd_762x51_AP_JCA_SMAG),
+        };
+    };
+
+    class CBA_762x51_SR25 {
+        ADDON[] = {
+            QCLASS(20Rnd_762x51_Ball_JCA_PMAG),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_PMAG),
+            QCLASS(20Rnd_762x51_EPR_JCA_PMAG),
+            QCLASS(20Rnd_762x51_AP_JCA_PMAG),
+            QCLASS(20Rnd_762x51_Ball_JCA_SMAG),
+            QCLASS(20Rnd_762x51_Ball_Tracer_JCA_SMAG),
+            QCLASS(20Rnd_762x51_EPR_JCA_SMAG),
+            QCLASS(20Rnd_762x51_AP_JCA_SMAG),
+        };
+    };
+};
