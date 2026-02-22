d407deaf4a1ab5412c3b81da6c091ab6e20514de
Brett Mayson <brett@mayson.io>
2026-02-22 00:15:11 -0600

    remove 556 g36

--- addons/weapons/script_macros.hpp
+++ addons/weapons/script_macros.hpp
@@ -1,13 +1,6 @@
 #define DESCRIPTION_AR_762x39 descriptionShort = "Assault Rifle<br />Caliber: 7.62x39 mm"
 #define DESCRIPTION_AR_556    descriptionShort = "Assault Rifle<br />Caliber: 5.56x45 mm"
 
-#define MAGAZINE_G36 \
-DESCRIPTION_AR_556;\
-magazines[] = {QCLASS(30Rnd_556x45_Ball_G36)};\
-magazineWell[] = {\
-    "CBA_556x45_G36",\
-}
-
 #define MAGAZINE_AKM \
 DESCRIPTION_AR_762x39;\
 magazines[] = {"30Rnd_762x39_AK12_Mag_F"};\
