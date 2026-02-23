eb10ba6d922485088f77eefd133e24c504556057
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-23 13:40:08 +0100

    jca: remove fixes applied in standarization

--- addons/ammunition/jca_556/script_component.hpp
+++
@@ -1,6 +0,0 @@
-#define COMPONENT ammunition_jca_556
-#define COMPONENT_BEAUTIFIED Ammunition JCA 556
-#include "\s\synixe_armoury\addons\main\script_mod.hpp"
-#include "\s\synixe_armoury\addons\main\script_macros.hpp"
-
-#include "..\script_macros.hpp"
