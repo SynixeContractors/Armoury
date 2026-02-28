f2d4aba48f15345c3b609185d6b94b6e70f83627
Patryk Kaniewski <p7tryk@gmail.com>
2026-02-28 18:29:36 +0100

    fix mp5 and ump dependencies (#20)
    * fix mp5 and ump dependencies
    
    * fix ump dependecny
    
    * mp5 change class names

--- addons/ammunition/jca_mp5a5/CfgMagazineWells.hpp
+++ addons/ammunition/jca_mp5a5/CfgMagazineWells.hpp
@@ -1,10 +1,10 @@
 class CfgMagazineWells {
-    class JCA_MP5_9x21 {
+    class JCA_MP5_9x19 {
         ADDON[] = {
-            QCLASS(30Rnd_9x21_Ball_MP5_JCA),
-            QCLASS(30Rnd_9x21_Ball_Tracer_MP5_JCA),
-            QCLASS(30Rnd_9x21_EPR_MP5_JCA),
-            QCLASS(30Rnd_9x21_AP_MP5_JCA)
+            QCLASS(30Rnd_9x19_Ball_MP5_JCA),
+            QCLASS(30Rnd_9x19_Ball_Tracer_MP5_JCA),
+            QCLASS(30Rnd_9x19_EPR_MP5_JCA),
+            QCLASS(30Rnd_9x19_AP_MP5_JCA)
         };
     };
 };
