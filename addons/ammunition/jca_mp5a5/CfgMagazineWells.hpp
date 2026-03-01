30f16395be5a1b1b2a6fa3de9e9f943f2e0b824d
Brett Mayson <brett@mayson.io>
2026-02-28 23:19:35 -0600

    fix mp5 magwell

--- addons/ammunition/jca_mp5a5/CfgMagazineWells.hpp
+++ addons/ammunition/jca_mp5a5/CfgMagazineWells.hpp
@@ -1,5 +1,5 @@
 class CfgMagazineWells {
-    class JCA_MP5_9x19 {
+    class JCA_MP5_9x21 {
         ADDON[] = {
             QCLASS(30Rnd_9x19_Ball_MP5_JCA),
             QCLASS(30Rnd_9x19_Ball_Tracer_MP5_JCA),
