b4620c6ce2fb3bb4e688793d438b99b9033af0b1
Patryk Kaniewski <p7tryk@gmail.com>
2026-03-07 13:08:02 +0100

    fix magwell and add aegis compatiblity to jca p320

--- addons/ammunition/jca_p320/CfgMagazineWells.hpp
+++ addons/ammunition/jca_p320/CfgMagazineWells.hpp
@@ -1,5 +1,5 @@
 class CfgMagazineWells {
-    class JCA_P320_9x19 {
+    class JCA_P320_Compact_9x19 {
         ADDON[] = {
             QCLASS(17Rnd_9x19_Ball_P320_JCA),
             QCLASS(17Rnd_9x19_Ball_Tracer_P320_JCA),
