de819d4f07ff1c16e51ab0dd6778d867ff5b67e0
Patryk Kaniewski <p7tryk@gmail.com>
2026-08-21 18:20:49 +0200

    fix conflicting modules

--- addons/launchers/rpg32/script_component.hpp
+++ addons/launchers/rpg32/script_component.hpp
@@ -1,6 +1,3 @@
-#define COMPONENT launchers
-#define COMPONENT_BEAUTIFIED Launchers RPG32
-#include "\s\synixe_armoury\addons\main\script_mod.hpp"
-#include "\s\synixe_armoury\addons\main\script_macros.hpp"
+#define SUBCOMPONENT rpg32
 
-#include "../script_macros.hpp"
+#include "..\script_component.hpp"
